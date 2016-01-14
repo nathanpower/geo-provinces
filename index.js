a7184ffcf1f11b6d517d01f7deb9a9228475a4c5
nathanpower-nearform <nathan@costadigital.io>
2016-01-14 10:29:58 +0000

    use Array.reduce to produce object rather than array

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ var request = require('request'),
     _ = require('lodash'),
     fs = require('fs');
 
-var countryISOCodes = ['IT', 'AT', 'CH', 'ES', 'FR', 'DE', 'AR'];
+var countryISOCodes = ['IT', 'AT', 'CH', 'ES', 'FR', 'DE', 'AR', 'GP', 'GY', 'MC', 'MQ', 'RE', 'SM', 'AD', 'GI'];
 var countryFunctions = [];
 countryISOCodes.map(function (isoCode) {
     countryFunctions.push(function (cb) {
         return request('http://api.geonames.org/countryInfo?username=nathanpower&country=' + isoCode + '&type=json', function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var result = JSON.parse(body);
+            var result = JSON.parse(body);
+            if (!error && response.statusCode == 200 && result.geonames[0]) {
                 cb(null, {"id": result.geonames[0].geonameId, "iso": isoCode});
+            } else {
+                throw new Error('Invalid response for ISO code ' + isoCode);
             }
         })
     })
@@ -29,14 +31,13 @@ async.series(countryFunctions, function (err, countryGeonameIds) {
         });
     });
     async.series(provinceFunctions, function (err, countryProvinces) {
-        var countryProvinceData = countryProvinces.map(function (provinces) {
-            var obj = {};
-            obj[provinces[0].countryCode] = [];
+        var countryProvinceData = countryProvinces.reduce(function (prevObj, provinces) {
+            prevObj[provinces[0].countryCode] = [];
             _.forEach(provinces, function (province) {
-                obj[provinces[0].countryCode].push(province.toponymName);
+                prevObj[provinces[0].countryCode].push(province.toponymName);
             });
-            return obj
-        });
+            return prevObj;
+        }, {});
         fs.writeFile('./provinces.json', JSON.stringify(countryProvinceData, null, 2), 'utf-8');
         console.log('finished!');
     });
